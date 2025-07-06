2c6f7486604fac27feeb7bcc0a0f8a89d06e32af
Gatopa <pepe2006manuel@gmail.com>
2025-07-06 16:57:37 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function openCard() {
+  document.querySelector('.cover').style.display = 'none';
+  document.querySelector('.card').classList.remove('hidden');
+  document.querySelector('.card').style.display = 'block';
+}
